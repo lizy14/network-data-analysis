c8bab7b7c4a80e4f2d912e87b00e61b99988470b
Li Zhaoyang <lizy14@yeah.net>
2015-11-02 08:23:57 +0000

    init for analyser

---
+++ analyser/GraphAnalyser/main.cpp
@@ -0,0 +1,15 @@
+/*
+文件名: main.cpp
+描　述: 入口点：图分析器
+
+作　者: 李肇阳, 清华大学软件学院, lizy14@yeah.net
+创建于: 2015-11-02
+
+环　境: Visual Studio 2012 (MSVC++ 11.0)
+*/
+#include <iostream>
+using namespace std;
+int main(int argc, char *argv[]){
+	
+	return 0;
+}
